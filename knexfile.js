e33b9862ccea52d0ff0c8d0e4633c40aede5c000
Erica Girges <ericafiorenza@gmail.com>
2020-10-16 08:45:18 -0700

    MVP progress

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,39 @@
+require("dotenv").config();
+
+const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";
+
 module.exports = {
   development: {
-    client: 'sqlite3',
-    connection: { filename: './database/auth.db3' },
+    client: "sqlite3",
     useNullAsDefault: true,
-    migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
+    connection: {
+      filename: "./database/auth.db3",
     },
-    seeds: { directory: './database/seeds' },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: pgConnection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
   },
 };
